445c57523c249735e83c7dffe17f6224bc9b0b92
Lucas Goncalves <gofontes.lucas@gmail.com>
2021-08-27 12:07:47 -0300

    ajuste main.tf

--- network/main.tf
+++ network/main.tf
@@ -29,6 +29,6 @@ module "privatertb" {
   source = "./privatertb"
   vpcid = module.vpc.myterraform_vpc_id
   natgwid = module.publicsubnets.myterraform_nat_gw_id
-  subnetida = module.publicsubnets.myterraform_privatesubnet_az_a_id
-  subnetidb = module.publicsubnets.myterraform_privatesubnet_az_b_id
+  pvt_subnetid_a = module.publicsubnets.myterraform_privatesubnet_az_a_id
+  pvt_subnetid_b = module.publicsubnets.myterraform_privatesubnet_az_b_id
 }
